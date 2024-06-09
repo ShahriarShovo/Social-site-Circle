3f8cc430e511f66be9b29551533297b1ec93f6fd
ShahriarShovo <developer.shahriar.rahaman@gmail.com>
2024-06-09 21:12:27 +0600

    added top comment,post and liked

--- a_users/views.py
+++ a_users/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.contrib import messages
 from django.urls import reverse
+from django.db.models import Count
+from a_post.views import ReplyCreateForm
 
 # Create your views here.
 @login_required
@@ -17,9 +19,28 @@ def profile_view(request, username=None):
             profile = request.user.profile
         except:
             raise Http404()
+        
+    posts = profile.user.posts.all()
+    
+    if request.htmx:
+        if 'top-posts' in request.GET:
+            posts = profile.user.posts.annotate(num_likes=Count('likes')).filter(num_likes__gt=0).order_by('-num_likes')
+        
+        elif 'top-comments' in request.GET:
+            comments = profile.user.comments.annotate(num_likes=Count('likes')).filter(num_likes__gt=0).order_by('-num_likes')
+            replyform = ReplyCreateForm()
+            return render(request, 'snippets/loop_profile_comments.html', {'comments':comments,'replyform': replyform })
+        
+        elif 'liked-posts' in request.GET:
+            posts = profile.user.likedposts.order_by('-likedpost__created') 
+    
+        return render(request, 'snippets/loop_profile_posts.html', { 'posts': posts })
+        
+    
     
     context={
-        'profile':profile
+        'profile':profile,
+        'posts':posts,
     }
     return render(request,'a_users/profile.html', context)
 
